98b5406ae0ec378c198d21225131bef2afb0cd39
Colton Berger <coltonberger@optonline.net>
2018-04-25 18:00:27 -0700

    added randomcolor

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+let body = document.getElementsByTagName("body")[0];
+
+  for (let i = 0; i < 63; i++) {
+    let div = document.createElement("div");
+    div.style.width = "11.1%"
+    div.style.float = "left"
+    div.style.paddingBottom = "11.1%"
+
+    function getRandomColor(){
+      let letters = '0123456789ABCDEF';
+      let color = '#';
+      for(let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+    div.style.backgroundColor = getRandomColor();
+
+  body.appendChild(div)
+}

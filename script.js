b324deec1290925973d701d42850bfa85f66753b
Colton Berger <coltonberger@optonline.net>
2018-04-26 09:48:44 -0700

    added gradient

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+let body = document.getElementsByTagName("body")[0];
+  body.style.background = "linear-gradient(#e58602, #f7d202)"
+
+  for (let i = 0; i < 63; i++) {
+    let div = document.createElement("div");
+    div.style.width = "11.1%"
+    div.style.float = "left"
+    div.style.paddingBottom = "11.1%"
+
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "#e58602"
+    div.style.opacity = "0.2"
+  } else {
+    div.style.backgroundColor = "#f7d202"
+    div.style.opacity = "0.2"
+  }
+
+  body.appendChild(div)
+}

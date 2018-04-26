49620720ed29f232efb4a7cf07237a745fa8418f
Colton Berger <coltonberger@optonline.net>
2018-04-25 17:49:01 -0700

    added red and black

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your JS goes here
+let body = document.getElementsByTagName("body")[0];
+
+  for (let i = 0; i < 63; i++) {
+    let div = document.createElement("div");
+    div.style.width = "11.1%"
+    div.style.float = "left"
+    div.style.paddingBottom = "11.1%"
+
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "black"
+  } else {
+    div.style.backgroundColor = "red"
+  }
+
+  body.appendChild(div)
+}
